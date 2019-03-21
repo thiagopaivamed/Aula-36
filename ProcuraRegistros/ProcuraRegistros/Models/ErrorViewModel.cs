2e709d87cb840c932ab2aa1aa3e66ddb5f2fb520
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-20 21:19:26 -0300

    Procura de registros

---
+++ ProcuraRegistros/ProcuraRegistros/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProcuraRegistros.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
